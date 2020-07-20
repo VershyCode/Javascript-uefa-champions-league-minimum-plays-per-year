0b8933354294a0a5f4889095ef3db77268bc8644
Paul G <paulsystem32@gmail.com>
2020-07-20 11:36:22 -0600

    Release v1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hola!
+
+La funcion recibira 2 argumentos, los cuales seran el año que deseamos buscar y el minimo de juegos jugados por equipo. 
+Por ejemplo si un equipo en el año 2015 jugo mas de 5 veces, y nuestro argumento pide que halla jugado al menos 4 veces, ese equipo será recolectado por la función.
+
+
+# Results
+Ejemplo: getTeams(2015, 5);
+![enter image description here](https://res.cloudinary.com/dlds4xwpk/image/upload/v1595266240/response_yzgs1p.png)
---
+++ for.js
@@ -0,0 +1,42 @@
+async function getTeams(year, k) {
+    try {
+        let page = 1;
+        let api = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&page=${page}`;
+        let teamsPlayed = []; 
+        let response = await fetch(api);
+        const { total_pages } = await response.json(); 
+        for(let i = 1; i <= total_pages; i++){
+            page = i;
+            api = `https://jsonmock.hackerrank.com/api/football_matches?competition=UEFA%20Champions%20League&year=${year}&page=${page}`;
+            response = await fetch(api);
+            /// [data] = response from [api] with the [page].
+            let { data } = await response.json();
+            // For each play in [data] (Plays of the year on [page]).
+            for(let juego of data){
+                // Save the name of the teams on [teamsPlayed].
+                teamsPlayed.push(juego.team1);
+                teamsPlayed.push(juego.team2);
+            }
+        }
+        let contados = {}; // [teamName] : [Number of duplicated values of [teamName] in [teamsPLayed]].
+        teamsPlayed.forEach((x) => { 
+            contados[x] = (contados[x] || 0)+1; 
+        });
+        let finalTeams = []; // [finalTeams] = the minimum(k) times that the [teamName] needs to be in [finalTeams].
+        for(let team in contados){
+            console.log(team, 'Played on ', year, contados[team], ' Times');
+            if(contados[team] >= k){
+                finalTeams.push(team);
+            }else{
+                continue;
+            }
+        }
+        console.warn('Results: ',{finalTeams});
+        return finalTeams;
+        
+    } catch (err) {
+        return err;
+    }
+}
+
+getTeams(2015, 5);
